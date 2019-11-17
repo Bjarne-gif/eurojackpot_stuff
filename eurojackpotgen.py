6bc185cd64369767e16b6c643dbfb49154d3966c
Bjarne-gif <57255094+Bjarne-gif@users.noreply.github.com>
2019-11-17 19:26:53 +0100

    Update eurojackpotgen.py

--- eurojackpotgen.py
+++ eurojackpotgen.py
@@ -1,8 +1,9 @@
 import random
 
 mylottonumbers= []
+eurozahlen= []
 
-for x in range (7):
+for x in range (5):
     randnumb = random.randint(1, 49)
     if randnumb not in mylottonumbers:
        mylottonumbers.append(randnumb)
@@ -11,3 +12,13 @@ for x in range (7):
         mylottonumbers.append(randnumbifexi)
     mylottonumbers.sort()
 print(mylottonumbers)
+
+for x in range (2):
+    randnumb = random.randint(1, 10)
+    if randnumb not in eurozahlen:
+       eurozahlen.append(randnumb)
+    else:
+        randnumbifexi = random.randint(1, 10)
+        eurozahlen.append(randnumbifexi)
+    eurozahlen.sort()
+print(eurozahlen)
