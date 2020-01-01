1335d77035472990436ac8e69169fdc4b29dcf59
Bjarne-gif <57255094+Bjarne-gif@users.noreply.github.com>
2020-01-01 16:45:48 +0100

    Update eurojackpotgen.py

--- eurojackpotgen.py
+++ eurojackpotgen.py
@@ -1,24 +1,13 @@
-import random
+#optimized best eurojackpot gen eu west
+from random import sample
 
-mylottonumbers= []
-eurozahlen= []
+mylottonumbers2 = []
+myeurojacknumbers2 = []
 
-for x in range (5):
-    randnumb = random.randint(1, 49)
-    if randnumb not in mylottonumbers:
-       mylottonumbers.append(randnumb)
-    else:
-        randnumbifexi = random.randint(1, 49)
-        mylottonumbers.append(randnumbifexi)
-    mylottonumbers.sort()
-print(mylottonumbers)
+mylottonumbers2 = sample(range(1, 49),5)
+mylottonumbers2_sortet = sorted(mylottonumbers2)
+print(mylottonumbers2_sortet)
 
-for x in range (2):
-    randnumb = random.randint(1, 10)
-    if randnumb not in eurozahlen:
-       eurozahlen.append(randnumb)
-    else:
-        randnumbifexi = random.randint(1, 10)
-        eurozahlen.append(randnumbifexi)
-    eurozahlen.sort()
-print(eurozahlen)
+myeurojacknumbers2 = sample(range(1, 10),2)
+myeurojacknumbers2_sortet = sorted(myeurojacknumbers2)
+print(myeurojacknumbers2_sortet)
