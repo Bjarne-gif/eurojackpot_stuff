ceefe2e80e2392cae180a5dad211009526b94531
Bjarne-gif <57255094+Bjarne-gif@users.noreply.github.com>
2020-03-08 21:33:53 +0100

    Update getvaluesfromtxtfiles.py

--- gewinnzahlen/getvaluesfromtxtfiles.py
+++ gewinnzahlen/getvaluesfromtxtfiles.py
@@ -15,42 +15,53 @@ for x in range(2011, 2021):
                 print("_______________count: "+str(counter))
                 mylist = ast.literal_eval(line) # Converting string to list 
                 print(mylist)
-                if counter is not 1:
-                    if counter is not 3:
+                if counter != 1:
+                    if counter != 3:
                         y = 0
-                        summeinsgesamt = 0
+                        sumtogether = 0
+                        arrayforpercentage = []
                         for y in range(1,51):
                             y = str(y) #int to str
                             #how often is a number in the list?
                             #print("Zahl: "+y+" -> "+ str(sum(x.count(y) for x in mylist)))
-                            summevonzahlinliste = str(sum(x.count(str(y)) for x in mylist))
-                            print("Zahl: "+y+" -> "+summevonzahlinliste)
+                            sumfromevvalue = str(sum(x.count(str(y)) for x in mylist))
+                            print("Zahl: "+y+" -> "+sumfromevvalue)
 
-                            #how much numbers are in the list?
-                            summeinsgesamt = int(summeinsgesamt) + int(summevonzahlinliste)
-                            
+                            #how many numbers are in the list?
+                            sumtogether = int(sumtogether) + int(sumfromevvalue)
+
+                            y = int(y)
+                            arrayforpercentage.insert(y, sumfromevvalue)
+
+                        print(arrayforpercentage)
+
                             #percentage of every number in the list in relation to all numbers
 
-
-                        print("Gesamt: "+str(summeinsgesamt))
+                        print("Gesamt: "+str(sumtogether))
                     else:
                         y = 0
-                        summeinsgesamt = 0
+                        sumtogether = 0
+                        arrayforpercentage = []
                         for y in range(1,11):
                             y = str(y) #int to str
                             #how often is a number in the list?
                             #print("Zahl: "+y+" -> "+ str(sum(x.count(y) for x in mylist)))
-                            summevonzahlinliste = str(sum(x.count(y) for x in mylist))
-                            print("Zahl: "+y+" -> "+summevonzahlinliste)
+                            sumfromevvalue = str(sum(x.count(y) for x in mylist))
+                            print("Zahl: "+y+" -> "+sumfromevvalue)
 
                             #how much numbers are in the list?
-                            summeinsgesamt = int(summeinsgesamt) + int(summevonzahlinliste)
+                            sumtogether = int(sumtogether) + int(sumfromevvalue)
 
-                            #percentage of every number in the list in relation to all numbers
+                            y = int(y)
+                            print(y)
                             
+                            arrayforpercentage.insert(y , sumfromevvalue)
+                                
+                        print(arrayforpercentage)
 
-                        print("Gesamt: "+str(summeinsgesamt))
-                            
+                            #percentage of every number in the list in relation to all numbers 
+
+                        print("Gesamt: "+str(sumtogether))                           
                 else:
                     exit
             f.close()
